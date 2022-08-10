26b2928ccf6a533075f2b5014533d4d02da4474b
LuPan2015 <lupan@kingsoft.com>
2022-08-10 21:01:35 +0800

    commit

--- flink-jobs/kafka2es/src/main/java/com/bigdata/util/Util.java
+++ flink-jobs/kafka2es/src/main/java/com/bigdata/util/Util.java
@@ -73,30 +73,6 @@ public class Util {
         return null;
     }
 
-    public static void test1 ()throws Exception{
-        //docker run --network=host --name fastdfs -v /data/fastdfs_data:/data -p 8080:8080 -e GO_FASTDFS_DIR=/data sjqzhang/go-fastdfs
-        String path = "E:\\code\\bigdata\\bigdata-001\\flink-jobs\\README.md";
-        InputStream inputStream = new FileInputStream(new File(path));
-        String p = "http://localhost:8080/group1/upload";
-        String name = "2.csv";
-        uploadFileToGOFastDFS(p,inputStream.readAllBytes(),name);
-    }
-
-    //https://towardsdatascience.com/hdfs-simple-docker-installation-guide-for-data-science-workflow-b3ca764fc94b
-    public static void test2() throws Exception {
-        //String url = "http://localhost:9870/webhdfs/v1/aaa/1.txt?op=OPEN&";
-        String url = "http://localhost:9864/webhdfs/v1/aaa/1.txt?op=OPEN&namenoderpcaddress=namenode:9000&offset=0";
-        byte[] inputStream = downloadFileFromHdfs(url);
-        System.out.println(new String(inputStream));
-        String p = "http://localhost:8080/group1/upload";
-        String name = "3.csv";
-        System.out.println(uploadFileToGOFastDFS(p,inputStream,name));
-    }
-
-    public static void main(String[] args)throws Exception{
-        test2();
-    }
-
     /**
      * 发送 post 请求
      * @param url
